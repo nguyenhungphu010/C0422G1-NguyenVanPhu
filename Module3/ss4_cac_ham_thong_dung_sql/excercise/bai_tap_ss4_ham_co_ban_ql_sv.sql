fce9694484b9891195e991538021790b7f62b58d
nguyenhungphu010 <nguyenhungphu010@gmail.com>
2022-07-19 22:41:23 +0700

    update

--- Module3/ss4_cac_ham_thong_dung_sql/excercise/bai_tap_ss4_ham_co_ban_ql_sv.sql
+++ Module3/ss4_cac_ham_thong_dung_sql/excercise/bai_tap_ss4_ham_co_ban_ql_sv.sql
@@ -4,20 +4,23 @@ use quanlysinhvien;
 -- sử dụng truy vấn con để sửa. 
 
 SELECT 
-    `subject`.SubId,
-    `subject`.SubName,
-    MAX(Credit) AS max_credit
+    *
 FROM
-    `subject`;
-
+    `subject`
+    WHERE 
+    Credit = (SELECT MAX(Credit)
+				 FROM `subject`);
 -- task 2: Hiển thị các thông tin môn học có điểm thi lớn nhất.
 -- sử dụng truy vấn con để sửa. 
 SELECT 
-    `subject`.SubId, `subject`.SubName, MAX(Mark) AS max_mark
+    `subject`.SubId, `subject`.SubName, mark.Mark
 FROM
     mark
         JOIN
-    `subject` ON `subject`.SubId = mark.SubId; 
+    `subject` ON `subject`.SubId = mark.SubId
+    WHERE 
+    Mark = (SELECT MAX(Mark) 
+              FROM  mark); 
     
 -- task 3: Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 
--- Module3/ss5_view_stored_procedured/exercise/bai_tap_ss5_database.sql
+++ Module3/ss5_view_stored_procedured/exercise/bai_tap_ss5_database.sql
@@ -9,11 +9,11 @@ productPrice double not null,
 
 productAmount int not null, 
 productDescription varchar(45), 
-productStatus varchar(45), 
+productStatus bit(1) default 0, 
     PRIMARY KEY (Id)
 );
-insert into Products (productCode, productName, productPrice,productAmount, productDescription, productStatus)
-values (123,"IPhone", 20000000, 5, "Apple Product", "Available"), 
-(124,"Galaxy", 21000000, 4, "Sumsung Product", "Available"), 
-(125,"Signature S", 200000000, 0, "Vertu Product", "Not Available"), 
-(126,"KEY2 Red", 1500000, 30, "BlackBerry Product", "Available"); 
+insert into Products (productCode, productName, productPrice,productAmount, productDescription)
+values (123,"IPhone", 20000000, 5, "Apple Product"), 
+(124,"Galaxy", 21000000, 4, "Sumsung Product"), 
+(125,"Signature S", 200000000, 0, "Vertu Product"), 
+(126,"KEY2 Red", 1500000, 30, "BlackBerry Product"); 
